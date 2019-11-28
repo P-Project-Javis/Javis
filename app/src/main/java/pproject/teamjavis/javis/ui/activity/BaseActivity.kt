ee6c56883ee00880f711416ef05abcea06272dc2
cheonjaewoong <55729813+cheonjaewoong@users.noreply.github.com>
2019-11-28 15:25:59 +0900

    BaseActivity 클래스 생성

---
+++ app/src/main/java/pproject/teamjavis/javis/ui/activity/BaseActivity.kt
@@ -0,0 +1,84 @@
+/*
+이 앱의 모든 액티비티가 상속할 베이스 액티비티 클래스
+
+기능:
+1. 액티비티 생명주기에 따라 로그를 찍음.(디버깅의 용이함을 위해 적용)
+2. 화면을 세로로 고정.
+3. sysout() 메소드를 통한 간편한 System.out.println() 가능.
+4. makeToast() 메소드를 통한 간편한 토스트 생성.
+5. makeSnackbar() 메소드를 통한 간편한 스낵바 생성.(확인 버튼이 있는 스낵바 생성 가능)
+ */
+package pproject.teamjavis.javis.ui.activity
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+open class BaseActivity: AppCompatActivity() {
+    private var Tag: String = this::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //액티비티 화면을 세로로 고정
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        Log.v(Tag, "onCreate() 실행")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.v(Tag, "onStart() 실행")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(Tag, "onResume() 실행")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(Tag, "onRestart() 실행")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(Tag, "onPause() 실행")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(Tag, "onStop() 실행")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(Tag, "onDestroy() 실행")
+    }
+
+    //System.out.println()을 간편하게 띄우는 메소드
+    fun sysout(text: String) {
+        println(text)
+    }
+
+    //토스트를 간편하게 띄우는 메소드
+    fun makeToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+    }
+
+    //스낵바를 간편하게 띄우는 메소드
+    fun makeSnackbar(view: View, message: String, useButton: Boolean) {
+        var duration = Snackbar.LENGTH_LONG
+        if(useButton)
+            duration = Snackbar.LENGTH_INDEFINITE
+        val snackbar = Snackbar.make(view, message, duration)
+        if(useButton) {
+            snackbar.setAction("확인") {
+                snackbar.dismiss()
+            }
+        }
+        snackbar.show()
+    }
+}
---
+++ app/src/main/java/pproject/teamjavis/javis/ui/activity/MainActivity.java
@@ -0,0 +1,13 @@
+package pproject.teamjavis.javis.ui.activity;
+
+import android.os.Bundle;
+
+import pproject.teamjavis.javis.R;
+
+public class MainActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
