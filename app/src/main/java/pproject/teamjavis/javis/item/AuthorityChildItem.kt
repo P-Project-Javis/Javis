4364486c786aacc996e93649182d7c7b3a4b7ee8
cheonjaewoong <55729813+cheonjaewoong@users.noreply.github.com>
2019-12-03 16:28:08 +0900

    커스텀 어댑터, 데이터클래스 생성

---
+++ app/src/main/java/pproject/teamjavis/javis/item/AuthorityChildItem.kt
@@ -0,0 +1,8 @@
+/*
+ExpandableListView의 자식 리스트 아이템 데이터 클래스.
+ */
+package pproject.teamjavis.javis.item
+
+import android.graphics.drawable.Drawable
+
+data class AuthorityChildItem(val icon: Drawable, val title: String)
