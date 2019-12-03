9b4da2bd095e1a316b5453f69eda6858bf5c5a9d
cheonjaewoong <cheonjaewoong@gmail.com>
2019-12-03 19:23:55 +0900

    부모, 자식 레이아웃 생성

--- app/src/main/java/pproject/teamjavis/javis/item/AuthorityChildItem.kt
+++ app/src/main/java/pproject/teamjavis/javis/item/AuthorityChildItem.kt
@@ -5,4 +5,4 @@ package pproject.teamjavis.javis.item
 
 import android.graphics.drawable.Drawable
 
-data class AuthorityChildItem(val icon: Drawable, val title: String)
+data class AuthorityChildItem(val icon: Drawable, val title: String, val isChecked: Boolean)
