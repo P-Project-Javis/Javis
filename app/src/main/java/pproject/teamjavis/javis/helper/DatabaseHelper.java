cb5d5da18a1a4677837f2630ee6ba22508970d52
jjimin327 <58248901+jjimin327@users.noreply.github.com>
2019-12-04 16:59:01 +0900

    Update DatabaseHelper.java

--- app/src/main/java/pproject/teamjavis/javis/helper/DatabaseHelper.java
+++ app/src/main/java/pproject/teamjavis/javis/helper/DatabaseHelper.java
@@ -1,61 +1,12 @@
 package pproject.teamjavis.javis.helper;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import pproject.teamjavis.javis.R;
 
-public class DatabaseHelper extends Activity {
-    EditText adduser_input;
-    DBHelper dbHelper;
-
-    final static String dbName = "user1.db";
-    final static int dbVersion = 2;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adduser);
-
-
-        adduser_input = (EditText) findViewById(R.id.adduser_input);
-        dbHelper = new DBHelper(this, dbName, null, dbVersion);
-
-    }
-    public void mOnClick(View v) {
-        SQLiteDatabase db;
-        String sql;
-
-        switch (v.getId()) {
-            case R.id.adduser_confirmButton: //추가 버튼(insert)
-                ImageButton ib = (ImageButton) findViewById(R.id.ImageButton);
-                String name = adduser_input.getText().toString();
-
-                db = dbHelper.getWritableDatabase();
-                sql = String.format("INSERT INTO t3 VALUES('" + name  + "',0);");
-
-                db.execSQL(sql);
-
-                Intent intent = new Intent(
-                        getApplicationContext(),
-                        taskview.class);
-                startActivity(intent);
-                Toast.makeText(getApplicationContext(), "저장되었습니다.", Toast.LENGTH_LONG).show();
-
-                break;
-        }
-        dbHelper.close();
-    }
+public class DatabaseHelper {
     static class DBHelper extends SQLiteOpenHelper {
 
         //생성자 - database 파일을 생성한다.
