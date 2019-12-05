55ede264a461b34085cf2a1a0f467767d322f167
cheonjaewoong <55729813+cheonjaewoong@users.noreply.github.com>
2019-12-05 16:27:15 +0900

    AdduserActivity 화면 이동 코드 추가

--- app/src/main/java/pproject/teamjavis/javis/activity/AdduserActivity.kt
+++ app/src/main/java/pproject/teamjavis/javis/activity/AdduserActivity.kt
@@ -12,6 +12,11 @@ import kotlinx.android.synthetic.main.layout_topbar.*
 import pproject.teamjavis.javis.R
 
 class AdduserActivity: BaseActivity() {
+    private var isNameChecked = false
+    private var isVoiceChecked = false
+
+    private var isRecording = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adduser)
@@ -20,5 +25,31 @@ class AdduserActivity: BaseActivity() {
         topbar_backButton.setOnClickListener { finish() }
 
         adduser_voiceWrap.visibility = View.GONE
+
+        adduser_nextButton.setOnClickListener {
+            adduser_nameWrap.visibility = View.GONE
+            adduser_voiceWrap.visibility = View.VISIBLE
+            isNameChecked = true
+            adduser_nameChecker.setImageDrawable(resources.getDrawable(R.drawable.ic_check_box_black_24dp))
+        }
+
+        adduser_recordButton.setOnClickListener {
+            if(!isRecording)
+                startRecording()
+            else
+                stopRecording()
+        }
+    }
+
+    private fun startRecording() {
+        adduser_recordButton.setImageDrawable(resources.getDrawable(R.drawable.ic_mic_black_48dp))
+        isRecording = true
+    }
+
+    private fun stopRecording() {
+        adduser_recordButton.setImageDrawable(resources.getDrawable(R.drawable.ic_mic_none_black_48dp))
+        isVoiceChecked = true
+        adduser_voiceChecker.setImageDrawable(resources.getDrawable(R.drawable.ic_check_box_black_24dp))
+        isRecording = false
     }
 }
