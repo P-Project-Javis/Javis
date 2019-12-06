f719bdf3b0797657c9d5ffef37090310fb9905e5
cheonjaewoong <55729813+cheonjaewoong@users.noreply.github.com>
2019-12-06 14:27:23 +0900

    activity_request를 activity_permisson으로 변경 및 안쓰는 drawable 삭제

---
+++ app/src/main/java/pproject/teamjavis/javis/activity/PermissionActivity.kt
@@ -0,0 +1,11 @@
+package pproject.teamjavis.javis.activity
+
+import android.os.Bundle
+import pproject.teamjavis.javis.R
+
+class PermissionActivity: BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_permission)
+    }
+}
