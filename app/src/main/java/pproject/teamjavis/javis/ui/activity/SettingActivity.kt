1d521482645e44fe87c186265176306ff7b30520
cheonjaewoong <55729813+cheonjaewoong@users.noreply.github.com>
2019-12-11 15:36:41 +0900

    사용자 설정 클릭시 팝업 레이아웃이 뜨도록 변경

--- app/src/main/java/pproject/teamjavis/javis/ui/activity/SettingActivity.kt
+++ app/src/main/java/pproject/teamjavis/javis/ui/activity/SettingActivity.kt
@@ -10,7 +10,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_setting.*
 import kotlinx.android.synthetic.main.layout_topbar.*
 import pproject.teamjavis.javis.R
-import pproject.teamjavis.javis.util.DatabaseHelper
+import pproject.teamjavis.javis.ui.popup.UserListPopup
 
 class SettingActivity: BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +20,25 @@ class SettingActivity: BaseActivity() {
 
         topbar_backButton.setOnClickListener { finish() }
 
-        setting_lookup.setOnClickListener { makeToast("조회") }
+        setting_lookup.setOnClickListener {
+            val popup = UserListPopup(applicationContext)
+            popup.setMode(UserListPopup.MODE_LOOKUP)
+            popup.pop(setting_parent)
+        }
         setting_adduser.setOnClickListener {
             val intent = Intent(applicationContext, AdduserActivity::class.java)
             startActivity(intent)
         }
-        setting_editUser.setOnClickListener { makeToast("수정") }
-        setting_removeUser.setOnClickListener { makeToast("삭제") }
-        setting_clearUser.setOnClickListener { makeToast("초기화") }
+        setting_editUser.setOnClickListener {
+            val popup = UserListPopup(applicationContext)
+            popup.setMode(UserListPopup.MODE_EDIT)
+            popup.pop(setting_parent)
+        }
+        setting_removeUser.setOnClickListener {
+            val popup = UserListPopup(applicationContext)
+            popup.setMode(UserListPopup.MODE_DELETE)
+            popup.pop(setting_parent)
+        }
 
         setting_appVersion.text = "Javis 버전: " + applicationContext.packageManager.getPackageInfo(applicationContext.packageName, 0).versionName
         setting_developer.setOnClickListener { makeToast("개발자") }
--- app/src/main/java/pproject/teamjavis/javis/ui/popup/UserListPopup.kt
+++ app/src/main/java/pproject/teamjavis/javis/ui/popup/UserListPopup.kt
@@ -1,2 +1,70 @@
 package pproject.teamjavis.javis.ui.popup
 
+import android.content.Context
+import android.graphics.Point
+import android.view.*
+import android.widget.LinearLayout
+import android.widget.PopupWindow
+import kotlinx.android.synthetic.main.layout_topbar.view.*
+import kotlinx.android.synthetic.main.layout_user_list.view.*
+import pproject.teamjavis.javis.R
+
+class UserListPopup(context: Context) {
+    companion object {
+        const val MODE_LOOKUP = 0
+        const val MODE_DELETE = 1
+        const val MODE_EDIT = 2
+
+        const val RETURN_WIDTH = 10
+        const val RETURN_HEIGHT = 11
+    }
+
+    private val context by lazy { context }
+    private val inflater by lazy { context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater }
+    private val popupView by lazy { inflater.inflate(R.layout.layout_user_list, null) }
+    private val popupWindow by lazy { PopupWindow(popupView, getDisplaySize(RETURN_WIDTH), getDisplaySize(RETURN_HEIGHT)) }
+
+    init {
+        popupView.isFocusable = false
+        popupView.user_list_topbar.topbar_backButton.setOnClickListener { popupWindow.dismiss() }
+    }
+
+    fun setMode(mode: Int) {
+        when(mode) {
+            MODE_LOOKUP -> {
+
+            }
+            MODE_DELETE -> {
+
+            }
+            MODE_EDIT -> {
+
+            }
+        }
+    }
+
+    fun pop(parent: View) {
+        popupWindow.showAtLocation(parent, Gravity.CENTER, 0, 0)
+    }
+
+    private fun getDisplaySize(mode: Int): Int {
+        val display = ((context.getSystemService(Context.WINDOW_SERVICE)) as WindowManager).defaultDisplay
+        val point = Point()
+        display.getSize(point)
+
+        val width = (point.x * 0.8).toInt()
+        val height = (point.y * 0.8).toInt()
+
+        return when (mode) {
+            RETURN_WIDTH -> {
+                width
+            }
+            RETURN_HEIGHT -> {
+                height
+            }
+            else -> {
+                LinearLayout.LayoutParams.MATCH_PARENT
+            }
+        }
+    }
+}
